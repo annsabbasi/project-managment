cc2aefac549eb9fd8a447cb2772ab9d85fe0d854
Anns Abbasi <128478344+annsabbasi@users.noreply.github.com>
2024-10-02 16:54:50 +0500

    Layout Typo Fix

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -2,8 +2,9 @@ import { Box } from "@mui/material";
 import Sidebar from './Sidebar';
 import TopBar from "./Topbar";
 import styles from './page.module.scss';
+import PropTypes from 'prop-types';
 
-export default function Layout(title, children) {
+export default function Layout({ title, children }) {
     console.log("This is a layout Page")
     return (
         <Box sx={{ display: 'flex' }}>
@@ -23,33 +24,7 @@ export default function Layout(title, children) {
     );
 }
 
-
-
-
-
-
-
-
-// import { Box } from "@mui/material";
-// import Sidebar from './Sidebar'
-// import TopBar from "./Topbar";
-// import styles from './page.module.scss'
-
-// export default function Layout( title, children ) {
-//     return (
-//         <Box sx={{ display: 'flex' }}>
-//             <Sidebar />
-//             <Box
-//                 sx={{
-//                     display: 'flex',
-//                     flexDirection: 'column',
-//                     height: '100vh'
-//                 }}
-//                 className={styles.container}
-//             >
-//                 <TopBar title={title} />
-//                 <Box>{children}</Box>
-//             </Box>
-//         </Box>
-//     )
-// }
+Layout.propTypes = {
+    title: PropTypes.string.isRequired, // Specify that title is required and must be a string
+    children: PropTypes.node.isRequired, // Specify that children is required and must be a valid React node
+}
