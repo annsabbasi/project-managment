cde6ba63a468351ad9c46c8ae7fdc4160c8a2b57
Anns Abbasi <128478344+annsabbasi@users.noreply.github.com>
2025-07-04 17:19:23 +0500

    running with the CORS local dev issues

--- server/src/app.js
+++ server/src/app.js
@@ -12,6 +12,7 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN.split(","),
+    // origin: "http://localhost:5173",
     credentials: true,
 }));
 app.use(express.json());
